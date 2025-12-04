8b7d2bcfbb09fe117ee45e22613c528dd521be48
gabriel72-g <130365528+gabriel72-g@users.noreply.github.com>
2025-12-04 10:51:42 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.mi-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-    map_customer_owned_ip_on_launch = true
+    map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
